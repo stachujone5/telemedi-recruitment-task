2624720417cdb082a5f0fc8d250064b57fdda8f0
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2023-08-23 21:00:33 +0200

    Frontend form validation, style fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Check, ClipboardCheck, Plus, X } from "lucide-react";
-import { KeyboardEvent, useRef } from "react";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const TASKS_QUERY_KEY = ["tasks"];
@@ -71,8 +72,23 @@ const deleteTaskFn = async (id: number) => {
 	return taskSchema.parse(data);
 };
 
+const taskFormSchema = z.object({
+	taskContent: z
+		.string()
+		.min(1, { message: "Task cannot be empty" })
+		.max(30, { message: "Max 30 characters" }),
+});
+
 export const App = () => {
-	const taskInputRef = useRef<HTMLInputElement>(null);
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<z.infer<typeof taskFormSchema>>({
+		resolver: zodResolver(taskFormSchema),
+	});
+
 	const queryClient = useQueryClient();
 	const {
 		data: tasks,
@@ -122,20 +138,6 @@ export const App = () => {
 		},
 	});
 
-	const handleAdd = () => {
-		if (taskInputRef.current) {
-			addTask(taskInputRef.current.value);
-			taskInputRef.current.value = "";
-		}
-	};
-
-	const handleTaskInputKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-		if (event.key === "Enter" && taskInputRef.current) {
-			addTask(taskInputRef.current.value);
-			taskInputRef.current.value = "";
-		}
-	};
-
 	const handleEdit = (id: number, done: boolean) => {
 		editTask({ id, done: !done });
 	};
@@ -144,78 +146,79 @@ export const App = () => {
 		deleteTask(id);
 	};
 
+	const onSubmit = (data: z.infer<typeof taskFormSchema>) => {
+		addTask(data.taskContent);
+		reset();
+	};
+
 	return (
-		<div className="flex h-screen w-screen items-center justify-center font-medium">
-			<div className="flex h-full flex-grow items-center justify-center bg-gray-100 text-gray-600">
-				<div className="flex h-full flex-grow items-center justify-center bg-gray-900">
-					<div className="flex h-96 w-96 max-w-full flex-col overflow-hidden rounded-lg bg-gray-800 p-8 text-gray-200 shadow-lg">
-						<div className="mb-6 flex items-center">
-							<ClipboardCheck className="text-blue-600" />
+		<div className="flex h-screen w-screen items-center justify-center bg-gray-900">
+			<div className="flex h-96 max-w-[24rem] flex-col overflow-hidden rounded-lg bg-gray-800 p-8 text-gray-200 shadow-lg">
+				<div className="mb-6 flex items-center">
+					<ClipboardCheck className="text-blue-600" />
 
-							<h4 className="ml-3 text-lg font-semibold">Tasks</h4>
-						</div>
-
-						{isError && (
-							<div className="flex flex-grow flex-col items-center justify-center text-center text-red-500">
-								<p>Something went wrong</p>
-								<p>Please try again later</p>
-							</div>
-						)}
-
-						{isLoading && <div className="flex-grow animate-pulse rounded bg-gray-900" />}
-
-						{tasks && tasks.length > 0 ? (
-							<div className="flex-grow overflow-y-auto">
-								{tasks.map((task) => (
-									<div key={task.id} className="flex h-10">
-										<input
-											className="hidden"
-											type="checkbox"
-											id={task.id.toString()}
-											checked={task.done}
-											onChange={() => handleEdit(task.id, task.done)}
-										/>
-										<label
-											className="flex h-10 flex-grow cursor-pointer items-center rounded px-2 transition-colors hover:bg-gray-900"
-											htmlFor={task.id.toString()}
-										>
-											<span className="flex h-5 w-5 items-center justify-center rounded-full border-2 border-gray-500 text-transparent">
-												<Check />
-											</span>
-											<span className="ml-4 text-sm">{task.content}</span>
-										</label>
-										<button
-											onClick={() => handleDelete(task.id)}
-											className="rounded-md p-2 transition-colors hover:bg-red-400"
-										>
-											<X />
-										</button>
-									</div>
-								))}
-							</div>
-						) : (
-							<div className="flex flex-grow items-center justify-center text-center text-white">
-								<p>No tasks to show</p>
-							</div>
-						)}
-
-						<div className="mt-2 flex items-center">
-							<input
-								className="h-8 flex-grow bg-transparent font-medium focus:outline-none"
-								type="text"
-								placeholder="add a new task"
-								ref={taskInputRef}
-								onKeyDown={handleTaskInputKeyDown}
-							/>
-							<button
-								className="rounded-md p-2 transition-colors hover:bg-green-500"
-								onClick={handleAdd}
-							>
-								<Plus />
-							</button>
-						</div>
-					</div>
+					<h4 className="ml-3 text-lg font-semibold">Tasks</h4>
 				</div>
+				{isError && (
+					<div className="flex flex-grow flex-col items-center justify-center text-center text-red-500">
+						<p>Something went wrong</p>
+						<p>Please try again later</p>
+					</div>
+				)}
+				{isLoading && <div className="flex-grow animate-pulse rounded bg-gray-900" />}
+				{tasks &&
+					(tasks.length > 0 ? (
+						<div className="flex-grow overflow-y-auto">
+							{tasks.map((task) => (
+								<div key={task.id} className="flex h-10">
+									<input
+										className="peer hidden"
+										type="checkbox"
+										id={task.id.toString()}
+										checked={task.done}
+										onChange={() => handleEdit(task.id, task.done)}
+									/>
+									<label
+										className="flex h-10 flex-grow cursor-pointer items-center rounded px-2 transition-colors hover:bg-gray-900"
+										htmlFor={task.id.toString()}
+									>
+										<span className="flex h-5 w-5 items-center justify-center rounded-full border-2 border-gray-500 text-transparent peer-checked:border-[#10b981] peer-checked:bg-[#10b981] peer-checked:text-white">
+											<Check />
+										</span>
+										<span className="ml-4 text-sm peer-checked:text-[#9ca3af] peer-checked:line-through">
+											{task.content}
+										</span>
+									</label>
+									<button
+										onClick={() => handleDelete(task.id)}
+										className="rounded-md p-2 transition-colors hover:bg-red-400"
+									>
+										<X />
+									</button>
+								</div>
+							))}
+						</div>
+					) : (
+						<div className="flex flex-grow items-center justify-center text-center text-white">
+							<p>No tasks to show</p>
+						</div>
+					))}
+				<form className="mt-2 flex items-center" onSubmit={handleSubmit(onSubmit)}>
+					<input
+						className="h-8 w-full bg-transparent font-medium focus:outline-none"
+						type="text"
+						placeholder="add a new task"
+						autoFocus={true}
+						{...register("taskContent")}
+					/>
+
+					<button className="rounded-md p-2 transition-colors hover:bg-green-500" type="submit">
+						<Plus />
+					</button>
+				</form>
+				{errors.taskContent?.message && (
+					<p className="text-xs text-red-500">{errors.taskContent.message}</p>
+				)}
 			</div>
 		</div>
 	);
